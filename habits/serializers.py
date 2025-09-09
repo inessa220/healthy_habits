70202bead733fe50224e30852d87b284f488dfde
inessa <inessa220@mail.ru>
2025-09-09 09:15:06 +0600

    validators

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 
 from habits.models import Habit
+from habits.validators import Validator
 
 
 class HabitSerializer(ModelSerializer):
@@ -8,3 +9,8 @@ class HabitSerializer(ModelSerializer):
         model = Habit
         fields = "__all__"
         read_only_fields = ["user"]
+
+    def validate(self, attrs):
+        validator = Validator()
+        validator(attrs)
+        return super().validate(attrs)
